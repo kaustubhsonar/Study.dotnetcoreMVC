7400b92eb520c6654069c20b2362b26dfdf80a8c
Kaustubh sonar <kaustubhsonar@outlook.com>
2020-06-10 22:40:06 +0530

    Created new controller with and without parameters

---
+++ Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Study.dotnetcoreMVC.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetBooks()
+        {
+            return "Getting all the books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Getting book with id = {id}";
+        }
+
+        //http://localhost:1180/book/searchbooks?name=jobs&authorname=kaustubh
+        public string SearchBooks(string name, string authorName)
+        {
+            return $"Getting book with name = {name} and author = {authorName}";
+        }
+    }
+}
