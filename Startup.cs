dd1636f64b5ca9d593ecee34c0b9cb88cb34a947
Kaustubh sonar <kaustubhsonar@outlook.com>
2020-06-11 20:00:28 +0530

    Added static files Created custom static files folder Configured static files middleware.

--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 
 namespace Study.dotnetcoreMVC
@@ -27,6 +29,11 @@ namespace Study.dotnetcoreMVC
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions() {
+            FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(),"AppStaticFiles")),
+            RequestPath = "/AppStaticFiles"
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
